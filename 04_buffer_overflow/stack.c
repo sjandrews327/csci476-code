edda931fe733b51284926084d91ad414a96eac56
Travis Peters <traviswp@gmail.com>
2020-02-01 23:48:55 -0700

    updating based on new resources

--- 04_buffer_overflow/stack.c
+++ 04_buffer_overflow/stack.c
@@ -1,28 +1,40 @@
-/* This program has a buffer overflow vulnerability. */
+/* Vunlerable program: stack.c */
+/* You can get this program from the lab's website */
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
-int foo(char *str)
+/* Changing this size will change the layout of the stack.
+ * Instructors can change this value each year, so students
+ * won't be able to use the solutions from the past.
+ * Suggested value: between 0 and 400  */
+#ifndef BUF_SIZE
+#define BUF_SIZE 24
+#endif
+
+int bof(char *str)
 {
-    char buffer[100];
+        char buffer[BUF_SIZE];
 
-    /* The following statement has a buffer overflow problem */
-    strcpy(buffer, str);
+        /* The following statement has a buffer overflow problem */
+        strcpy(buffer, str);
 
-    return 1;
+        return 1;
 }
 
 int main(int argc, char **argv)
 {
-    char str[400];
-    FILE *badfile;
+        char str[517];
+        FILE *badfile;
 
-    badfile = fopen("badfile", "r");
-    fread(str, sizeof(char), 300, badfile);
-    foo(str);
+        /* Change the size of the dummy array to randomize the parameters
+           for this lab. Need to use the array at least once */
+        char dummy[BUF_SIZE];  memset(dummy, 0, BUF_SIZE);
 
-    printf("Returned Properly\n");
-    return 1;
+        badfile = fopen("badfile", "r");
+        fread(str, sizeof(char), 517, badfile);
+        bof(str);
+        printf("Returned Properly\n");
+        return 1;
 }
-
