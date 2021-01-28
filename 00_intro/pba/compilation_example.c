88e7711dbf89a83ac1effeec85743562b5873d3a
Travis Peters <traviswp@gmail.com>
2021-01-28 12:51:51 -0700

    add another compiled example (w/ gdb debug symbols)

--- 00_intro/pba/compilation_example.c
+++ 00_intro/pba/compilation_example.c
@@ -3,9 +3,7 @@
 #define FORMAT_STRING "%s"
 #define MESSAGE       "Hello, world!\n"
 
-int
-main(int argc, char *argv[]) {
-  printf(FORMAT_STRING, MESSAGE);
-  return 0;
+int main (int argc, char *argv[]) {
+    printf(FORMAT_STRING, MESSAGE);
+    return 0;
 }
-
