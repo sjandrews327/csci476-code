ebe79c344c1df04ab9c4e6a9e654576f2d5bbe8b
Travis Peters <traviswp@gmail.com>
2021-02-01 23:36:15 -0700

    cleaning up

--- 01_setuid/cap_leak.c
+++ 01_setuid/cap_leak.c
@@ -5,28 +5,27 @@
 
 void main()
 {
-  int fd;
-  char *v[2];
+    int fd;
+    char *v[2];
 
-  /* Assume that /etc/zzz is an important system file,
-   * and it is owned by root with permission 0644.
-   * Before running this program, you should create
-   * the file /etc/zzz first. */
-  fd = open("/etc/zzz", O_RDWR | O_APPEND);  
-  if (fd == -1) {
-     printf("Cannot open /etc/zzz\n");
-     exit(0);
-  }
+    /* Assume that /etc/zzz is an important system file,
+     * and it is owned by root with permission 0644.
+     * Before running this program, you should create
+     * the file /etc/zzz first.
+     */
+    fd = open("/etc/zzz", O_RDWR | O_APPEND);
+    if (fd == -1) {
+        printf("Cannot open /etc/zzz\n");
+        exit(0);
+    }
 
-  // Print out the file descriptor value
-  printf("fd is %d\n", fd);
+    // Print out the file descriptor value
+    printf("fd is %d\n", fd);
 
-  // Permanently disable the privilege by making the
-  // effective uid the same as the real uid
-  setuid(getuid());         
+    // Permanently disable elevated privileges by making EUID == RUID
+    setuid(getuid());
 
-  // Execute /bin/sh
-  v[0] = "/bin/sh"; v[1] = 0;
-  execve(v[0], v, 0);        
+    // Execute /bin/sh
+    v[0] = "/bin/sh"; v[1] = 0;
+    execve(v[0], v, 0);
 }
-
