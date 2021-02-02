df4bbee025f839705209315fe53c74dabb633b03
Travis Peters <traviswp@gmail.com>
2021-02-01 20:42:11 -0700

    reformat

--- 02_envvars/myprintenv.c
+++ 02_envvars/myprintenv.c
@@ -1,7 +1,8 @@
 // Compile:
 //  $ gcc myprintenv.c -o myprintenv
+//
 // Run the program and redirect output to a text file:
-//  $ myprintenv > file
+//  $ ./myprintenv > myenv1
 
 #include <unistd.h>
 #include <stdio.h>
@@ -11,22 +12,22 @@ extern char **environ;
 
 void printenv()
 {
-  int i = 0;
-  while (environ[i] != NULL) {
-     printf("%s\n", environ[i]);
-     i++;
-  }
+    int i = 0;
+    while (environ[i] != NULL) {
+        printf("%s\n", environ[i]);
+        i++;
+    }
 }
 
 void main()
 {
-  pid_t childPid;
-  switch(childPid = fork()) {
-    case 0:  /* child process */
-      printenv();
-      exit(0);
+    pid_t childPid;
+    switch(childPid = fork()) {
+    case 0:   /* child process */
+        printenv();
+        exit(0);
     default:  /* parent process */
-      // printenv();
-      exit(0);
-  }
+        // printenv();
+        exit(0);
+    }
 }
