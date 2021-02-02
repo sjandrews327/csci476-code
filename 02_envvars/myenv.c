9579057268f6d157e86f8bdb6adcda6ba86ef787
Travis Peters <traviswp@gmail.com>
2021-02-01 20:52:26 -0700

    update myenv

--- 02_envvars/myenv.c
+++ 02_envvars/myenv.c
@@ -1,16 +1,17 @@
+// Compile:
+//  $ gcc myenv.c -o myenv
+
 #include <unistd.h>
 
 extern char **environ;
 
 int main()
 {
-  char *argv[2];
+    char *argv[2];
+    argv[0] = "/usr/bin/env";
+    argv[1] = NULL;
 
-  argv[0] = "/usr/bin/env";
-  argv[1] = NULL;
+    execve(argv[0], argv, NULL);
 
-  execve("/usr/bin/env", argv, NULL);  
-
-  return 0 ;
+    return 0;
 }
-
