fe992757697823f14c3b6d864c3d4dda9a7aa6c0
Travis Peters <traviswp@gmail.com>
2021-02-01 23:44:06 -0700

    update example

--- 01_setuid/cap_leak.c
+++ 01_setuid/cap_leak.c
@@ -6,9 +6,9 @@
 void main()
 {
     int fd;
-    char *v[2];
 
-    /* Assume that /etc/zzz is an important system file,
+    /*
+     * Assume that /etc/zzz is an important system file,
      * and it is owned by root with permission 0644.
      * Before running this program, you should create
      * the file /etc/zzz first.
@@ -19,13 +19,26 @@ void main()
         exit(0);
     }
 
-    // Print out the file descriptor value
-    printf("fd is %d\n", fd);
+    // Simulate the tasks conducted by the program
+    sleep(1);
 
-    // Permanently disable elevated privileges by making EUID == RUID
+    /*
+     * After the task, elevated privileges are no longer needed;
+     * it is time to relinquish these privileges!
+     * NOTE: getuid() returns the real UID (RUID)
+     */
     setuid(getuid());
 
-    // Execute /bin/sh
-    v[0] = "/bin/sh"; v[1] = 0;
-    execve(v[0], v, 0);
+    if (fork()) {  /* parent process */
+        close (fd);
+        exit(0);
+    } else {  /* child process */
+
+        /*
+         * Now, assume that the child process is compromised, and that
+         * malicious attackers have injected the following statements into this process
+         */
+        write (fd, "Malicious Data\n", 15);
+        close (fd);
+    }
 }
