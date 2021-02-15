b993d67189866505c03d2193f4f6136ba4d6a6e1
Travis Peters (SEED VM) <traviswp@gmail.com>
2021-02-14 17:35:48 -0700

    fix include

--- 03_buffer_overflow/demos/simpleshell.c
+++ 03_buffer_overflow/demos/simpleshell.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-// #include <sys/types.h>
-// #include <unistd.h>
+#include <unistd.h>
 
 int main()
 {
-        char *argv[2];
-        argv[0] = "/bin/sh";
-        argv[1] = NULL;
-        execve(argv[0], argv, NULL);
-        return 0;
+    char *argv[2];
+    argv[0] = "/bin/sh";
+    argv[1] = NULL;
+    execve(argv[0], argv, NULL);
+    return 0;
 }
