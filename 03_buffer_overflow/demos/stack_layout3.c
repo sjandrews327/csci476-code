c907e1bb72ad287752726ea07ba464da19a741ae
Travis Peters (SEED VM) <traviswp@gmail.com>
2021-02-25 20:15:17 -0700

    add variable to show address in mmap too

--- 03_buffer_overflow/demos/stack_layout3.c
+++ 03_buffer_overflow/demos/stack_layout3.c
@@ -2,14 +2,23 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 int main() 
 {
-	char x[12];
-	char *y = malloc(sizeof(char)*12);
+    char x[12];
+    void *y = &malloc;
+    char *z = malloc(sizeof(char)*12);
 
-	printf("Address of buffer x (on stack): 0x%x\n", (unsigned int)x);
-	printf("Address of buffer y (on heap):  0x%x\n", (unsigned int)y);
-	
-	return 0;
+    printf("Address of x (in stack): 0x%x\n", (unsigned int)x);
+    printf("Address of y (in mmap):  0x%x\n", (unsigned int)y);
+    printf("Address of z (in heap):  0x%x\n", (unsigned int)z);
+
+    // while(1) {
+    //     sleep(10);
+    //     printf(".");
+    //     fflush(stdout);
+    // }
+
+    return 0;
 }
