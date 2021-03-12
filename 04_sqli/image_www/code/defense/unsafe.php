b730bfed3e888bb55658ea1bb1f13eb40e25e255
Travis Peters <traviswp@gmail.com>
2021-03-12 10:39:03 -0700

    tweaks to make more consistent with how our solution is laid out

--- 04_sqli/image_www/code/defense/unsafe.php
+++ 04_sqli/image_www/code/defense/unsafe.php
@@ -1,12 +1,12 @@
 <?php
-// Function to create a sql connection.
+// function to create a sql connection.
 function getDB() {
   $dbhost="10.9.0.6";
   $dbuser="seed";
   $dbpass="dees";
   $dbname="sqllab_users";
 
-  // Create a DB connection
+  // create a DB connection
   $conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
   if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error . "\n");
@@ -14,19 +14,19 @@ function getDB() {
   return $conn;
 }
 
+// create a connection
+$conn = getDB();
+
 $input_uname = $_GET['username'];
 $input_pwd = $_GET['password'];
 $hashed_pwd = sha1($input_pwd);
-
-// create a connection
-$conn = getDB();
 
 // do the query
 $result = $conn->query("SELECT id, name, eid, salary, ssn
                         FROM credential
                         WHERE name= '$input_uname' and password= '$hashed_pwd'");
 if ($result->num_rows > 0) {
-  // only take the first row 
+  // only take the first row
   $firstrow = $result->fetch_assoc();
   $id     = $firstrow["id"];
   $name   = $firstrow["name"];
