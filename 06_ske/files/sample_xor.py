09630b7d36d8bd6c3ece4b47a420ed6e6a479a9a
Travis Peters <traviswp@gmail.com>
2021-04-01 20:00:16 -0600

    cleaning up the sample code a bit

--- 06_ske/files/sample_xor.py
+++ 06_ske/files/sample_xor.py
@@ -4,20 +4,17 @@
 # XOR strings (ascii strings and hex strings).
 #
 
-# XOR two bytearrays
-def xor(first, second):
-   return bytearray(x^y for x,y in zip(first, second))
-
 MSG   = "A message"
 HEX_1 = "aabbccddeeff1122334455"
 HEX_2 = "1122334455778800aabbdd"
 
-# Convert ascii string to bytearray
-D1 = bytes(MSG, 'utf-8')
+# XOR two bytearrays
+def xor(first, second):
+   return bytearray(x^y for x,y in zip(first, second))
 
-# Convert hex string to bytearray
-D2 = bytearray.fromhex(HEX_1)
-D3 = bytearray.fromhex(HEX_2)
+D1 = bytes(MSG, 'utf-8')      # Convert ascii string to bytearray
+D2 = bytearray.fromhex(HEX_1) # Convert hex string to bytearray
+D3 = bytearray.fromhex(HEX_2) # Convert hex string to bytearray
 
 r1 = xor(D1, D2)
 r2 = xor(D2, D3)
